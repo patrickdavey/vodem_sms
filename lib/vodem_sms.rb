6b251d85195d1ee1eae61c7b99ea33632afd8d40
Patrick Davey <patrick.davey@gmail.com>
2015-04-26 14:35:12 +1200

    Adding stub Vodem class

--- lib/vodem_sms.rb
+++ lib/vodem_sms.rb
@@ -1,5 +1,20 @@
 require "vodem_sms/version"
+require "typhoeus"
+require "pry"
 
 module VodemSms
-  # Your code goes here...
+  class Vodem
+    def initialize
+      setup
+    end
+
+
+    private
+
+    attr_reader :current_status
+
+    def setup
+
+    end
+  end
 end
---
+++ spec/lib/vodem_sms_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+require 'vodem_sms'
+
+module VodemSms
+  describe Vodem do
+
+  end
+end
