e06f655c502c8a0051842b06679d69e64a1ff7e3
Patrick Davey <patrick.davey@gmail.com>
2015-04-26 15:11:19 +1200

    Refactoring status out to a status checker.
    Probably ought to return a status object really

--- lib/vodem_sms.rb
+++ lib/vodem_sms.rb
@@ -1,4 +1,5 @@
 require "vodem_sms/version"
+require "vodem_sms/status_checker"
 require "typhoeus"
 require "pry"
 require "json"
@@ -8,15 +9,6 @@ module VodemSms
     CONNECTED    = "ppp_connected"
     DISCONNECTED = "ppp_disconnected"
     CONNECTING   = "ppp_connecting"
-
-    WEBSERVER_STATUS_URL    = "http://192.168.9.1/goform/goform_get_cmd_process"
-    WEBSERVER_HOST_HEADER    = "192.168.9.1"
-    AGENT_HEADER = "User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:37.0) Gecko/20100101 Firefox/37.0'" 
-    LANGUAGE_HEADER= "en-US,en;q=0.5"
-    ENCODING_HEADER = 'gzip, deflate'
-    X_REQUESTED_WITH_HEADER = 'XMLHttpRequest'
-    REFERRER_HEADER = 'http://192.168.9.1/home.htm'
-    STATUS_FIELD = 'ppp_status'
 
     def initialize
       setup
@@ -31,7 +23,7 @@ module VodemSms
     end
 
     def connecting?
-      current_status == DISCONNECTED
+      current_status == CONNECTING
     end
 
 
@@ -44,20 +36,7 @@ module VodemSms
     end
 
     def set_status
-      response = Typhoeus.get(
-        WEBSERVER_STATUS_URL,
-        headers: {
-          Host: WEBSERVER_HOST_HEADER,
-          "User-Agent" => AGENT_HEADER,
-          "Accept-Language" => LANGUAGE_HEADER,
-          "Accept-Encoding" => ENCODING_HEADER,
-          "x-requested-with" => X_REQUESTED_WITH_HEADER,
-          "Referer" => REFERRER_HEADER,
-          Accept: "application/json"
-        },
-        params: {cmd: "ppp_status"},
-      )
-      @current_status = JSON.parse(response.body)[STATUS_FIELD]
+      @current_status ||= StatusChecker.new.get_status
     end
   end
 end
--- spec/lib/vodem_sms_spec.rb
+++ spec/lib/vodem_sms_spec.rb
@@ -4,10 +4,44 @@ require 'vodem_sms'
 module VodemSms
   describe Vodem do
     context "disconnected status" do
+      before do
+        stub_request(:get, "http://192.168.9.1/goform/goform_get_cmd_process?cmd=ppp_status").
+          to_return(status: 200, body: '{"ppp_status": "ppp_disconnected"}')
+      end
       let(:vodem) { Vodem.new }
 
       it "must correctly setup the connection status" do
         expect(vodem.disconnected?).to be(true)
+        expect(vodem.connected?).to be(false)
+        expect(vodem.connecting?).to be(false)
+      end
+    end
+
+    context "connected status" do
+      before do
+        stub_request(:get, "http://192.168.9.1/goform/goform_get_cmd_process?cmd=ppp_status").
+          to_return(status: 200, body: '{"ppp_status": "ppp_connected"}')
+      end
+      let(:vodem) { Vodem.new }
+
+      it "must correctly setup the connection status" do
+        expect(vodem.disconnected?).to be(false)
+        expect(vodem.connected?).to be(true)
+        expect(vodem.connecting?).to be(false)
+      end
+    end
+
+    context "connecting status" do
+      before do
+        stub_request(:get, "http://192.168.9.1/goform/goform_get_cmd_process?cmd=ppp_status").
+          to_return(status: 200, body: '{"ppp_status": "ppp_connecting"}')
+      end
+      let(:vodem) { Vodem.new }
+
+      it "must correctly setup the connection status" do
+        expect(vodem.disconnected?).to be(false)
+        expect(vodem.connected?).to be(false)
+        expect(vodem.connecting?).to be(true)
       end
     end
   end
