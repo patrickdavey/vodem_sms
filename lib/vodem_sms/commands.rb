f97d890a04eddd9b3887b5d503d87881beefbcdd
Patrick Davey <patrick.davey@gmail.com>
2015-05-06 21:15:25 +1200

    DRYing up headers in commands

--- lib/vodem_sms/commands.rb
+++ lib/vodem_sms/commands.rb
@@ -5,17 +5,7 @@ module VodemSms
     def connect!
       Typhoeus.post(
         WEBSERVER_SET_CMD_URL,
-        headers: {
-          Host: WEBSERVER_HOST_HEADER,
-          "User-Agent" => AGENT_HEADER,
-          "Accept-Language" => LANGUAGE_HEADER,
-          "Accept-Encoding" => ENCODING_HEADER,
-          "Cache-Control" => CACHE_CONTROL_HEADER,
-          "Pragma" => PRAGMA_HEADER,
-          "x-requested-with" => X_REQUESTED_WITH_HEADER,
-          "Referer" => REFERRER_HEADER,
-          Accept: "application/json"
-        },
+        headers: common_headers,
         timeout: 3,
         body: {goformId: "CONNECT_NETWORK"},
       )
@@ -25,18 +15,9 @@ module VodemSms
     def delete_message(id)
       Typhoeus.post(
         WEBSERVER_SET_CMD_URL,
-        headers: {
-          Host: WEBSERVER_HOST_HEADER,
-          "User-Agent" => AGENT_HEADER,
-          "Accept-Language" => LANGUAGE_HEADER,
-          "Accept-Encoding" => ENCODING_HEADER,
-          "Cache-Control" => CACHE_CONTROL_HEADER,
+        headers: common_headers.merge({
           "Content-Type" => CONTENT_TYPE_HEADER,
-          "Pragma" => PRAGMA_HEADER,
-          "x-requested-with" => X_REQUESTED_WITH_HEADER,
-          "Referer" => REFERRER_HEADER,
-          Accept: "application/json"
-        },
+        }),
         body: {
           goformId: "DELETE_SMS",
           msg_id: id
@@ -47,21 +28,28 @@ module VodemSms
     def disconnect!
       Typhoeus.post(
         WEBSERVER_SET_CMD_URL,
-        headers: {
-          Host: WEBSERVER_HOST_HEADER,
-          "User-Agent" => AGENT_HEADER,
-          "Accept-Language" => LANGUAGE_HEADER,
-          "Accept-Encoding" => ENCODING_HEADER,
-          "Cache-Control" => CACHE_CONTROL_HEADER,
-          "Pragma" => PRAGMA_HEADER,
-          "x-requested-with" => X_REQUESTED_WITH_HEADER,
-          "Referer" => REFERRER_HEADER,
-          Accept: "application/json"
-        },
+        headers: common_headers,
         body: {goformId: "DISCONNECT_NETWORK"},
       )
 
       sleep 2
+    end
+
+
+    private
+
+    def common_headers
+      {
+        Host: WEBSERVER_HOST_HEADER,
+        "User-Agent" => AGENT_HEADER,
+        "Accept-Language" => LANGUAGE_HEADER,
+        "Accept-Encoding" => ENCODING_HEADER,
+        "Cache-Control" => CACHE_CONTROL_HEADER,
+        "Pragma" => PRAGMA_HEADER,
+        "x-requested-with" => X_REQUESTED_WITH_HEADER,
+        "Referer" => REFERRER_HEADER,
+        Accept: "application/json"
+      }
     end
   end
 end
